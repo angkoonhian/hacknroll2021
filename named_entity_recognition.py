76e92c8b7dadcf5637d69a135e33616ff82ed8e2
aliciaajy <65158683+aliciaajy@users.noreply.github.com>
2021-01-08 21:03:17 +0800

    updated ner

--- named_entity_recognition.py
+++ named_entity_recognition.py
@@ -9,45 +9,54 @@ Original file is located at
 # Using Spacy Package for Named Entity Recognition
 """
 
+!pip install flask Flask-Markdown spacy
+
+from flask import Flask,url_for,render_template,request
+from flaskext.markdown import Markdown
 import numpy as np
 import nltk, re
-from nltk.util import ngrams
 import spacy
 from spacy.lang.en import English
-from nltk.stem import WordNetLemmatizer 
 from nltk.tokenize import sent_tokenize
-import PyPDF2
-from tika import parser
-from pathlib import Path
-
-sp = spacy.load('en_core_web_sm')
-
-
-text = sp('''
-New York (CNN)When Liana Barrientos was 23 years old, she got married in Westchester County, New York.
-A year later, she got married again in Westchester County, but to a different man and without divorcing her first husband.
-Only 18 days after that marriage, she got hitched yet again. Then, Barrientos declared "I do" five more times, sometimes only within two weeks of each other.
-In 2010, she married once more, this time in the Bronx. In an application for a marriage license, she stated it was her "first and only" marriage.
-Barrientos, now 39, is facing two criminal counts of "offering a false instrument for filing in the first degree," referring to her false statements on the
-2010 marriage license application, according to court documents.
-Prosecutors said the marriages were part of an immigration scam.
-On Friday, she pleaded not guilty at State Supreme Court in the Bronx, according to her attorney, Christopher Wright, who declined to comment further.
-After leaving court, Barrientos was arrested and charged with theft of service and criminal trespass for allegedly sneaking into the New York subway through an emergency exit, said Detective
-Annette Markowski, a police spokeswoman. In total, Barrientos has been married 10 times, with nine of her marriages occurring between 1999 and 2002.
-All occurred either in Westchester County, Long Island, New Jersey or the Bronx. She is believed to still be married to four men, and at one time, she was married to eight men at once, prosecutors say.
-Prosecutors said the immigration scam involved some of her husbands, who filed for permanent residence status shortly after the marriages.
-Any divorces happened only after such filings were approved. It was unclear whether any of the men will be prosecuted.
-The case was referred to the Bronx District Attorney\'s Office by Immigration and Customs Enforcement and the Department of Homeland Security\'s
-Investigation Division. Seven of the men are from so-called "red-flagged" countries, including Egypt, Turkey, Georgia, Pakistan and Mali.
-Her eighth husband, Rashid Rajput, was deported in 2006 to his native Pakistan after an investigation by the Joint Terrorism Task Force.
-If convicted, Barrientos faces up to four years in prison.  Her next court appearance is scheduled for May 18.
-''')
-
-for entity in text.ents:
-    print(entity.text + ' - ' + entity.label_ + ' - ' + str(spacy.explain(entity.label_)))
-
 from spacy import displacy
+import json
 
-filter = {'ents':['GPE', 'ORG', 'PERSON']}
-displacy.render(text, style='ent', jupyter=True, options = filter)
+app = Flask(__name__)
+Markdown(app)
+HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+
+@app.route('/extract',methods=["GET","POST"])
+def extract():
+  nlp = spacy.load('en_core_web_sm')
+  filter = {'ents':['GPE', 'ORG', 'PERSON']}
+  if request.method == 'POST':
+    raw_text = request.form['rawtext']
+    docx = nlp(raw_text) 
+    html = displacy.render(docx,style="ent", jupyter=True, options = filter)
+    html = html.replace("\n\n","\n")
+    result = HTML_WRAPPER.format(html)
+  return render_template('result.html',rawtext=raw_text,result=result)
+
+
+@app.route('/previewer')
+def previewer():
+	return render_template('previewer.html')
+
+@app.route('/preview',methods=["GET","POST"])
+def preview():
+	if request.method == 'POST':
+		newtext = request.form['newtext']
+		result = newtext
+	return render_template('preview.html',newtext=newtext,result=result)
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
+
 
